423d18a4965de4f5c229a6814d666e6aede17eba
Lilian Besson <lilian.besson@crans.org>
2019-09-09 16:04:13 +0200

    Add matplotlib, tqdm and numpy as requirements (so the Binder page works well). Cf. https://mybinder.org/

--- CoursMagistral_1.py
+++ CoursMagistral_1.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 # # Table of Contents
-#  <p><div class="lev1 toc-item"><a href="#ALGO1-:-Introduction-à-l'algorithmique" data-toc-modified-id="ALGO1-:-Introduction-à-l'algorithmique-1"><span class="toc-item-num">1&nbsp;&nbsp;</span><a href="https://perso.crans.org/besson/teach/info1_algo1_2019/" target="_blank">ALGO1 : Introduction à l'algorithmique</a></a></div><div class="lev1 toc-item"><a href="#Cours-Magistral-1" data-toc-modified-id="Cours-Magistral-1-2"><span class="toc-item-num">2&nbsp;&nbsp;</span>Cours Magistral 1</a></div><div class="lev2 toc-item"><a href="#Listes-simplement-chaînées" data-toc-modified-id="Listes-simplement-chaînées-21"><span class="toc-item-num">2.1&nbsp;&nbsp;</span>Listes simplement chaînées</a></div><div class="lev3 toc-item"><a href="#pop/push-pour-une-structure-de-file-(FILO)" data-toc-modified-id="pop/push-pour-une-structure-de-file-(FILO)-211"><span class="toc-item-num">2.1.1&nbsp;&nbsp;</span><code>pop</code>/<code>push</code> pour une structure de file (FILO)</a></div><div class="lev3 toc-item"><a href="#add/remove-pour-une-structure-de-pile-(FIFO)" data-toc-modified-id="add/remove-pour-une-structure-de-pile-(FIFO)-212"><span class="toc-item-num">2.1.2&nbsp;&nbsp;</span><code>add</code>/<code>remove</code> pour une structure de pile (FIFO)</a></div><div class="lev2 toc-item"><a href="#Listes-doublement-chaînées" data-toc-modified-id="Listes-doublement-chaînées-22"><span class="toc-item-num">2.2&nbsp;&nbsp;</span>Listes doublement chaînées</a></div><div class="lev3 toc-item"><a href="#Exemple" data-toc-modified-id="Exemple-221"><span class="toc-item-num">2.2.1&nbsp;&nbsp;</span>Exemple</a></div><div class="lev2 toc-item"><a href="#Implémenter-une-file-d'attente-avec-deux-piles" data-toc-modified-id="Implémenter-une-file-d'attente-avec-deux-piles-23"><span class="toc-item-num">2.3&nbsp;&nbsp;</span>Implémenter une file d'attente avec deux piles</a></div><div class="lev2 toc-item"><a href="#File-de-priorité-:-implémentation-naïve" data-toc-modified-id="File-de-priorité-:-implémentation-naïve-24"><span class="toc-item-num">2.4&nbsp;&nbsp;</span>File de priorité : implémentation naïve</a></div><div class="lev2 toc-item"><a href="#Implémentation-d'un-tas-binaire" data-toc-modified-id="Implémentation-d'un-tas-binaire-25"><span class="toc-item-num">2.5&nbsp;&nbsp;</span>Implémentation d'un tas binaire</a></div><div class="lev3 toc-item"><a href="#Naïve-:-tableau-trié-!" data-toc-modified-id="Naïve-:-tableau-trié-!-251"><span class="toc-item-num">2.5.1&nbsp;&nbsp;</span>Naïve : tableau trié !</a></div><div class="lev3 toc-item"><a href="#Intelligente-:-tas-binaire-min-équilibré" data-toc-modified-id="Intelligente-:-tas-binaire-min-équilibré-252"><span class="toc-item-num">2.5.2&nbsp;&nbsp;</span>Intelligente : tas binaire min équilibré</a></div><div class="lev2 toc-item"><a href="#Tri-par-tas" data-toc-modified-id="Tri-par-tas-26"><span class="toc-item-num">2.6&nbsp;&nbsp;</span>Tri par tas</a></div><div class="lev3 toc-item"><a href="#Remarques" data-toc-modified-id="Remarques-261"><span class="toc-item-num">2.6.1&nbsp;&nbsp;</span>Remarques</a></div><div class="lev3 toc-item"><a href="#Tri-par-tas" data-toc-modified-id="Tri-par-tas-262"><span class="toc-item-num">2.6.2&nbsp;&nbsp;</span>Tri par tas</a></div><div class="lev2 toc-item"><a href="#Test-numérique-de-l'efficacité-du-tri-par-tas" data-toc-modified-id="Test-numérique-de-l'efficacité-du-tri-par-tas-27"><span class="toc-item-num">2.7&nbsp;&nbsp;</span>Test numérique de l'efficacité du tri par tas</a></div>
+#  <p><div class="lev1 toc-item"><a href="#ALGO1-:-Introduction-à-l'algorithmique" data-toc-modified-id="ALGO1-:-Introduction-à-l'algorithmique-1"><span class="toc-item-num">1&nbsp;&nbsp;</span><a href="https://perso.crans.org/besson/teach/info1_algo1_2019/" target="_blank">ALGO1 : Introduction à l'algorithmique</a></a></div><div class="lev1 toc-item"><a href="#Cours-Magistral-1" data-toc-modified-id="Cours-Magistral-1-2"><span class="toc-item-num">2&nbsp;&nbsp;</span>Cours Magistral 1</a></div><div class="lev2 toc-item"><a href="#Listes-simplement-chaînées" data-toc-modified-id="Listes-simplement-chaînées-21"><span class="toc-item-num">2.1&nbsp;&nbsp;</span>Listes simplement chaînées</a></div><div class="lev3 toc-item"><a href="#pop/push-pour-une-structure-de-file-(FILO)" data-toc-modified-id="pop/push-pour-une-structure-de-file-(FILO)-211"><span class="toc-item-num">2.1.1&nbsp;&nbsp;</span><code>pop</code>/<code>push</code> pour une structure de file (FILO)</a></div><div class="lev3 toc-item"><a href="#add/remove-pour-une-structure-de-pile-(FIFO)" data-toc-modified-id="add/remove-pour-une-structure-de-pile-(FIFO)-212"><span class="toc-item-num">2.1.2&nbsp;&nbsp;</span><code>add</code>/<code>remove</code> pour une structure de pile (FIFO)</a></div><div class="lev2 toc-item"><a href="#Listes-doublement-chaînées" data-toc-modified-id="Listes-doublement-chaînées-22"><span class="toc-item-num">2.2&nbsp;&nbsp;</span>Listes doublement chaînées</a></div><div class="lev3 toc-item"><a href="#Exemple" data-toc-modified-id="Exemple-221"><span class="toc-item-num">2.2.1&nbsp;&nbsp;</span>Exemple</a></div><div class="lev2 toc-item"><a href="#Implémenter-une-file-d'attente-avec-deux-piles" data-toc-modified-id="Implémenter-une-file-d'attente-avec-deux-piles-23"><span class="toc-item-num">2.3&nbsp;&nbsp;</span>Implémenter une file d'attente avec deux piles</a></div><div class="lev2 toc-item"><a href="#File-de-priorité-:-implémentation-naïve" data-toc-modified-id="File-de-priorité-:-implémentation-naïve-24"><span class="toc-item-num">2.4&nbsp;&nbsp;</span>File de priorité : implémentation naïve</a></div><div class="lev2 toc-item"><a href="#Implémentation-d'un-tas-binaire" data-toc-modified-id="Implémentation-d'un-tas-binaire-25"><span class="toc-item-num">2.5&nbsp;&nbsp;</span>Implémentation d'un tas binaire</a></div><div class="lev3 toc-item"><a href="#Naïve-:-tableau-trié-!" data-toc-modified-id="Naïve-:-tableau-trié-!-251"><span class="toc-item-num">2.5.1&nbsp;&nbsp;</span>Naïve : tableau trié !</a></div><div class="lev3 toc-item"><a href="#Intelligente-:-tas-binaire-min-équilibré" data-toc-modified-id="Intelligente-:-tas-binaire-min-équilibré-252"><span class="toc-item-num">2.5.2&nbsp;&nbsp;</span>Intelligente : tas binaire min équilibré</a></div><div class="lev2 toc-item"><a href="#Tri-par-tas" data-toc-modified-id="Tri-par-tas-26"><span class="toc-item-num">2.6&nbsp;&nbsp;</span>Tri par tas</a></div><div class="lev3 toc-item"><a href="#Remarques" data-toc-modified-id="Remarques-261"><span class="toc-item-num">2.6.1&nbsp;&nbsp;</span>Remarques</a></div><div class="lev3 toc-item"><a href="#Tri-par-tas" data-toc-modified-id="Tri-par-tas-262"><span class="toc-item-num">2.6.2&nbsp;&nbsp;</span>Tri par tas</a></div><div class="lev2 toc-item"><a href="#Test-numérique-de-l'efficacité-du-tri-par-tas" data-toc-modified-id="Test-numérique-de-l'efficacité-du-tri-par-tas-27"><span class="toc-item-num">2.7&nbsp;&nbsp;</span>Test numérique de l'efficacité du tri par tas</a></div><div class="lev2 toc-item"><a href="#Évaluation-numérique-de-la-complexité-des-opérations-du-tas-binaire" data-toc-modified-id="Évaluation-numérique-de-la-complexité-des-opérations-du-tas-binaire-28"><span class="toc-item-num">2.8&nbsp;&nbsp;</span>Évaluation numérique de la complexité des opérations du tas binaire</a></div><div class="lev3 toc-item"><a href="#Sur-un-seul-exemple" data-toc-modified-id="Sur-un-seul-exemple-281"><span class="toc-item-num">2.8.1&nbsp;&nbsp;</span>Sur un seul exemple</a></div>
 
 # # [ALGO1 : Introduction à l'algorithmique](https://perso.crans.org/besson/teach/info1_algo1_2019/)
 # 
@@ -629,10 +629,11 @@ heapSort(example_array)
 insertionSort(example_array)
 
 
-# In[203]:
+# In[237]:
 
 
 example_array = list(range(2019)) + list(range(2019))  # twice the numbers from 0 to 2018
+import random
 random.shuffle(example_array)
 
 get_ipython().run_line_magic('timeit', 'sorted(example_array)')
@@ -691,7 +692,7 @@ import timeit
 try:
     from tqdm import tqdm_notebook
 except ImportError:
-    def tqdm_notebook(iterator): return iterator
+    def tqdm_notebook(iterator, *args, **kwargs): return iterator
 
 
 # In[216]:
@@ -714,8 +715,9 @@ def time_a_sort_function(sort_function, sort_function_name, values_n, number=100
 
 small_values_n = [10, 100, 500] + list(range(1000, 5000, 1000))
 big_values_n = list(range(6000, 100000, 4000))
+very_big_values_n = list(range(100000, 5000000, 100000))
 
-values_n = small_values_n + big_values_n
+values_n = small_values_n + big_values_n + very_big_values_n
 
 
 # In[234]:
@@ -766,4 +768,94 @@ get_ipython().run_line_magic('timeit', 'sorted(random_array_of_int(max_int=10, l
 get_ipython().run_line_magic('timeit', 'sorted(random_array_of_int(max_int=1000, length=100))')
 get_ipython().run_line_magic('timeit', 'sorted(random_array_of_int(max_int=1000, length=1000))')
 get_ipython().run_line_magic('timeit', 'sorted(random_array_of_int(max_int=1000, length=10000))')
+
+
+# ## Évaluation numérique de la complexité des opérations du tas binaire
+# 
+# On peut évaluer, sur des exemples, la complexité des opérations d'ajout et d'extraction du maximum que l'on a implémenté dans notre structure de tas binaire.
+# On a montré qu'elles sont toutes les deux en $\mathcal{O}(\log(n))$, mais peut-on le vérifier empiriquement ?
+
+# In[266]:
+
+
+import numpy as np
+
+
+# In[291]:
+
+
+def times_push_and_pop(values_n, number1=100, number2=100, max_int=1000_000):
+    # create a random array for each value of n
+    times_push = np.array([ np.mean([
+        timeit.timeit(
+            "heap.push(random.randint(-{}, {}))".format(max_int, max_int),
+            globals={
+                'random_array_of_int': random_array_of_int,
+                'heap': heap,
+                'random': random,
+            },
+            number=number1,
+        ) / number1 for _ in range(number2)
+        for heap in [ OurHeap(random_array_of_int(max_int=max_int, length=n)) ]
+        ])
+       for n in tqdm_notebook(values_n, desc="push")
+    ])
+    times_push_and_pop = np.array([  np.mean([
+        timeit.timeit(
+        "heap.push(random.randint(-{}, {})); heap.pop()".format(max_int, max_int),
+        globals={
+            'random_array_of_int': random_array_of_int,
+            'heap': heap,
+            'random': random,
+        },
+        number=number1,
+        ) / number1 for _ in range(number2)
+        for heap in [ OurHeap(random_array_of_int(max_int=max_int, length=n)) ]
+        ])
+       for n in tqdm_notebook(values_n, desc="push & pop")
+    ])
+    times_pop = times_push_and_pop - times_push
+    return times_push, times_pop
+
+
+# In[292]:
+
+
+times_push_and_pop([10, 100, 1000], number1=100, number2=1000)
+
+
+# In[294]:
+
+
+def visualisation_temps_push_and_pop(values_n, **kwargs):
+    times_push, times_pop = times_push_and_pop(values_n, **kwargs)
+    plt.figure()
+    plt.xlabel("Taille du tableau d'entrée $n$")
+    plt.ylabel("Temps en micro-secondes")
+    plt.title("Temps des opérations ajouterMax et retirerMax")
+    plt.plot(values_n, 1e6 * times_push, "d-", label="ajouterMax", lw=5, ms=12)
+    plt.plot(values_n, 1e6 * times_pop, "o-", label="retierMax", lw=5, ms=12)
+    plt.legend()
+    plt.show()
+
+
+# In[301]:
+
+
+visualisation_temps_push_and_pop(
+    [
+        100, 500, 1000, 2000, 3000, 4000, 5000,
+        10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000,
+        #110000, 120000, 130000, 140000, 150000, 160000, 170000, 180000, 190000, 200000,
+        #300000, 400000, 500000, 600000, 700000, 800000, 900000,
+        #1000000, 2000000, 3000000, 4000000, 5000000, 6000000, 7000000, 8000000, 9000000,
+    ],
+    number1=100, number2=10000,
+)
+
+
+# In[ ]:
+
+
+
 
